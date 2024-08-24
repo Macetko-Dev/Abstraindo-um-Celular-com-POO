1eff33ead66e0ad25b21112665d3f4cb08cf29a1
Macetko-Dev <macetko@gmail.com>
2024-08-24 10:56:25 -0300

    Concluido git add Models/Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -8,7 +8,7 @@ namespace DesafioPOO.Models
         private int Memoria {get; set;}
 
         
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama.
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
@@ -16,7 +16,7 @@ namespace DesafioPOO.Models
             Modelo = modelo;
             IMEI = imei;
             Memoria = memoria;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            // TODO: Passar os parâmetros do construtor para as propriedades.
         }
 
         public void Ligar()
